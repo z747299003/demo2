1bcad9380a7e43309ef06cd14f59b7694c5ae613
yong.zhang <747299003@qq.com>
2022-06-28 02:06:51 +0800

    链表反转

---
+++ src/main/java/org/example/algorithm/class04/Code01_ReverseList.java
@@ -0,0 +1,63 @@
+package org.example.algorithm.class04;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Code01_ReverseList {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        ListNode listNode = reverseList(n1);
+        while(listNode!=null){
+            System.out.println(listNode.val);
+            listNode=listNode.next;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+}
